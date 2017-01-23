3b74f38cc8d8436375fe055e929d300fb5930707
djmarcelino <DaveTemp@Ferbruary.local>
2017-01-23 21:58:57 +0800

    v3
    footer and map fix

--- js/main.js
+++ js/main.js
@@ -7,10 +7,10 @@ if(document.getElementById("overlay").style.visibility == "visible")
 {
     document.getElementById("overlay").style.visibility = "hidden";
     document.getElementById("overlay").style.opacity = "0";
-    document.getElementById("logo").style.color = "#fff";
-    document.getElementById("bar1").style.backgroundColor = "#fff";
-    document.getElementById("bar2").style.backgroundColor = "#fff";
-    document.getElementById("bar3").style.backgroundColor = "#fff";
+    document.getElementById("logo").style.color = "#FFFFFF";
+    document.getElementById("bar1").style.backgroundColor = "#FFFFFF";
+    document.getElementById("bar2").style.backgroundColor = "#FFFFFF";
+    document.getElementById("bar3").style.backgroundColor = "#FFFFFF";
     
     if($(window).scrollTop() > 1) {
         $(".headerwrapper").addClass("active");
@@ -25,10 +25,254 @@ else
 {   
     document.getElementById("overlay").style.visibility = "visible";
     document.getElementById("overlay").style.opacity = "1";
-    document.getElementById("logo").style.color = "#272d3d";
-    document.getElementById("bar1").style.backgroundColor = "#272d3d";
-    document.getElementById("bar2").style.backgroundColor = "#272d3d";
-    document.getElementById("bar3").style.backgroundColor = "#272d3d";
+    document.getElementById("logo").style.color = "#F47920";
+    document.getElementById("bar1").style.backgroundColor = "#F47920";
+    document.getElementById("bar2").style.backgroundColor = "#F47920";
+    document.getElementById("bar3").style.backgroundColor = "#F47920";
     $('#header').css('padding', '32px 0');
     $(".headerwrapper").removeClass("active");
 }}
+
+
+
+
+
+
+
+
+
+function initMap() {
+                var Latlng = {lat: 1.279896, lng: 103.855430};
+                var map = new google.maps.Map(document.getElementById('googlemapwrapper'), {
+                    zoom: 17,
+                    center: Latlng,
+                    gestureHandling: 'cooperative',
+                    scrollwheel: false,
+                    navigationControl: false,
+                    mapTypeControl: false,
+                    scaleControl: false,
+                    draggable: true,
+                    styles: [
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#ebe3cd"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#523735"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#f5f1e6"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#c9b2a6"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#dcd2be"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#ae9e90"
+      }
+    ]
+  },
+  {
+    "featureType": "landscape.natural",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#dfd2ae"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#dfd2ae"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#93817c"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#a5b076"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#447530"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#f5f1e6"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#fdfcf8"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#f8c967"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#e9bc62"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway.controlled_access",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e98d58"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway.controlled_access",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#db8555"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#806b63"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#dfd2ae"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#8f7d77"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#ebe3cd"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#dfd2ae"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#b9d3c2"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#92998d"
+      }
+    ]
+  }
+]
+
+                });
+                var marker = new google.maps.Marker({
+                    lat: 1.2798475,
+                    lng: 103.8532363,
+                    position: Latlng,
+                    map: map
+                });
+              }
